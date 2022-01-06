7d63f21f58212eebaaf23d0f608efa51a4bc7103
Lillie <75392499+iLillie@users.noreply.github.com>
2022-01-06 14:24:11 +0100

    feat: Add model

---
+++ assignments/week-1/1-mvc-todo/js/model.js
@@ -0,0 +1,47 @@
+const model = {
+  // 1: app
+  app: {
+    page: "home",
+  },
+
+  // 2: inputs
+  inputs: {},
+
+  // 3: shared data
+  tasks: [
+    {
+      id: 0,
+      authorId: 0,
+      content: "Buy groceries",
+      isCompleted: true,
+      creationDate: new Date(),
+      completionDate: null,
+    },
+    {
+      id: 1,
+      authorId: 0,
+      content: "Make dinner",
+      isCompleted: false,
+      creationDate: new Date(),
+      completionDate: null,
+    },
+    {
+      id: 2,
+      authorId: 1,
+      content: "Daily coding",
+      isCompleted: false,
+      creationDate: new Date(),
+      completionDate: null,
+    },
+  ],
+  users: [
+    {
+      id: 0,
+      name: "Lillie",
+    },
+    {
+      id: 1,
+      name: "Terje",
+    },
+  ],
+};
